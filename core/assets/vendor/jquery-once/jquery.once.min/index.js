31c08db0a9c83db55e4032a2577852a9889ce134
Aron Novak <aron@gizra.com>
2020-04-05 08:01:24 +0200

    minification

--- core/assets/vendor/jquery-once/jquery.once.min/index.js
+++ core/assets/vendor/jquery-once/jquery.once.min/index.js
@@ -1,8 +1,1 @@
-/*!
- * jQuery Once v2.2.0 - http://github.com/robloach/jquery-once
- * @license MIT, GPL-2.0
- *   http://opensource.org/licenses/MIT
- *   http://opensource.org/licenses/GPL-2.0
- */
-(function(e){"use strict";if(typeof exports==="object"){e(require("jquery"))}else if(typeof define==="function"&&define.amd){define(["jquery"],e)}else{e(jQuery)}})(function(e){"use strict";var n=function(e){e=e||"once";if(typeof e!=="string"){throw new TypeError("The jQuery Once id parameter must be a string")}return e};e.fn.once=function(t){var r="jquery-once-"+n(t);return this.filter(function(){return e(this).data(r)!==true}).data(r,true)};e.fn.removeOnce=function(e){return this.findOnce(e).removeData("jquery-once-"+n(e))};e.fn.findOnce=function(t){var r="jquery-once-"+n(t);return this.filter(function(){return e(this).data(r)===true})}});
-//# sourceMappingURL=jquery.once.min.js.map
+!function(e){"use strict";"object"==typeof exports?e(require("jquery")):"function"==typeof define&&define.amd?define(["jquery"],e):e(jQuery)}(function(t){"use strict";function r(e){if("string"!=typeof(e=e||"once"))throw new TypeError("The jQuery Once id parameter must be a string");return e}t.fn.once=function(e){var n="jquery-once-"+r(e);return this.filter(function(){return!0!==t(this).data(n)}).data(n,!0)},t.fn.removeOnce=function(e){return this.findOnce(e).removeData("jquery-once-"+r(e))},t.fn.findOnce=function(e){var n="jquery-once-"+r(e);return this.filter(function(){return!0===t(this).data(n)})}});
